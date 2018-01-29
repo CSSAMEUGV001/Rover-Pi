d5337df5a977f78cfce3032019dc41907099f632
Farid Chahla <farid.chahla@gmail.com>
2018-01-29 15:31:13 -0800

    added simple video streaming

--- listener.py
+++ listener.py
@@ -1,11 +1,15 @@
 import atexit
 import threading
 import logging
+from io import BytesIO
+from base64 import b64encode
+from picamera import PiCamera
 from socketIO_client import SocketIO, BaseNamespace
 from i2c_backend import PyCar
 
 KEY = b'0kXMZqwpoAgRUqOXk2Tjsubd1qndPyGR'
 HOST = '192.168.1.87'
+#HOST = 'muchomath.us.to'
 PORT = 5000
 
 logging.getLogger('socketIO-client').setLevel(logging.INFO)
@@ -23,26 +27,61 @@ def neutralize():
 class RoverNamespace(BaseNamespace):
     def __init__(self, *args, **kwargs):
         BaseNamespace.__init__(self, *args, **kwargs)
-        self.timer = threading.Timer(1.0, neutralize)
-        self.timer.start()
+        self.camera = PiCamera()
+        self.camera.resolution = (384, 216)
+        self.buffer = BytesIO()
+        self.neutralize_timer = None
+        self.capture_timer = None
+        self.continue_capture = True
+        self.start_neutralize_timer()
+
+    def start_neutralize_timer(self):
+        self.neutralize_timer = threading.Timer(1.0, neutralize)
+        self.neutralize_timer.start()
+
+    def start_capture_timer(self):
+        self.capture_timer = threading.Timer(0.25, self.capture)
+        self.capture_timer.start()
+
+    def capture(self):
+        self.camera.capture(self.buffer, 'jpeg')
+        stream = self.buffer.getvalue()
+        data = b64encode(stream).decode()
+        self.emit('image', data)
+        self.buffer.seek(0)
+        self.buffer.truncate()
+        if self.continue_capture:
+            self.start_capture_timer()
+        else:
+            self.capture_timer = None
 
     def on_connect(self):
         logger.info('connected')
+        self.continue_capture = True
+        if not self.capture_timer:
+            self.start_capture_timer()
 
     def on_reconnect(self):
         logger.info('reconnected')
+        self.continue_capture = True
+        if not self.capture_timer:
+            self.start_capture_timer()
 
     def on_disconnect(self):
         logger.info('disconnected')
         neutralize()
+        self.continue_capture = False
 
     def on_control(self, message):
-        self.timer.cancel()
+        if self.neutralize_timer:
+            self.neutralize_timer.cancel()
         steering = message.get('steering', 90)
         throttle = message.get('throttle', 90)
-        car.control(steering, throttle)
-        self.timer = threading.Timer(1.0, neutralize)
-        self.timer.start()
+        try:
+            car.control(steering, throttle)
+        except:
+            logger.error('Car connection dropped')
+        self.start_neutralize_timer()
 
 socketIO = SocketIO(HOST, PORT)
 rover_namespace = socketIO.define(RoverNamespace, '/rover')
